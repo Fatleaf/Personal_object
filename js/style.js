025171e8243e8d5e3d259d4177d5adfb9ad02c5f
Fatleaf <cycfree1988@gmail.com>
2020-12-31 01:59:13 +0800

    update
    rwd完成、基本JS練習

--- js/style.js
+++ js/style.js
@@ -1,12 +1,10 @@
+//navBig效果
 var section01 = document.querySelector('#section01')
 var section03 = document.querySelector('#section03')
 var section04 = document.querySelector('#section04')
 var nav = document.querySelector('nav')
 var list = nav.querySelectorAll('li')
 var nav_a = nav.querySelectorAll('a')
-
-console.log(nav_a);
-
 
 
 window.onscroll = function () {
@@ -16,21 +14,16 @@ window.onscroll = function () {
         nav_a[index].classList.remove('longer')
     }
 
-
     var scroll_now = document.documentElement.scrollTop
-    // console.log(nav_a[2]);
-    console.log(list[0]);
 
     if (scroll_now <= section02.offsetTop/2) {
         nav_a[0].classList.add('longer')
-        // list[0].innerHTML = `你好`
     }
     else if (scroll_now <= (section03.offsetTop + section02.offsetTop)/2) {
         nav_a[1].classList.add('longer')
     }
     else if (scroll_now <= (section04.offsetTop + section03.offsetTop)/2) {
         nav_a[2].classList.add('longer')
-        // nav_a[2].classList.add('hide')
     }
     else if (scroll_now <= (section05.offsetTop + section04.offsetTop)/2) {
         nav_a[3].classList.add('longer')
@@ -41,12 +34,45 @@ window.onscroll = function () {
     else {
         nav_a[5].classList.add('longer')
     }
-
-    // console.log(section03.clientTop);
 }
 
 $(function(){
     checkName();
+
+    $('#nav_phone ul').hide();
+
+    $('#nav_phone').on('mouseenter', function(){
+        $('#nav_phone ul').css('display', 'block');
+    });
+    $('#nav_phone').on('mouseleave', function(){
+        $('#nav_phone ul').css('display', 'none');
+    });
+
+    //一進入就判斷視窗寬度
+    if (document.body.clientWidth > 768) {
+        $('#navBig').show();
+        $('#nav_phone').hide();
+    } else {
+        $('#nav_phone').show();
+        $('#navBig').hide();
+        $('#section03 .photo').attr('data-aos', 'fade-up');
+        $('#section03 .photo').attr('data-aos-anchor-placement', 'top-bottom');
+    }
+    
+  
+    //即時監控視窗寬度顯示NAV
+    $(window).resize(function() {
+
+        reWidth = $(window).width();
+        if (reWidth > 768) {
+            $('#nav_phone').hide();
+            $('#navBig').show();
+        }else{
+            $('#nav_phone').show();
+            $('#navBig').hide();
+        }
+    });
+
 });
 
 //判斷input暱稱
@@ -59,10 +85,6 @@ function checkName(){
         }
         });
 };
-
-// var sec03_photo = $('#section03 .photo') ;//想辦法把data-aos效果用JS移除
-
-// console.log();
 
 
 // for (let index = 0; index < list.length; index++) {
