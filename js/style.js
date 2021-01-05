804a5fb2b4ca3d68069231f4039733417ffd71a3
Fatleaf <cycfree1988@gmail.com>
2021-01-05 18:53:52 +0800

    update
    Fetch 天氣卡片

--- js/style.js
+++ js/style.js
@@ -6,35 +6,40 @@ var nav = document.querySelector('nav')
 var list = nav.querySelectorAll('li')
 var nav_a = nav.querySelectorAll('a')
 
+function activeNav (){
+    window.onscroll = function () {
 
-window.onscroll = function () {
-
-    for (let index = 0; index < nav_a.length; index++) {
-        const element = nav_a[index];
-        nav_a[index].classList.remove('longer')
-    }
-
-    var scroll_now = document.documentElement.scrollTop
-
-    if (scroll_now <= section02.offsetTop/2) {
-        nav_a[0].classList.add('longer')
-    }
-    else if (scroll_now <= (section03.offsetTop + section02.offsetTop)/2) {
-        nav_a[1].classList.add('longer')
-    }
-    else if (scroll_now <= (section04.offsetTop + section03.offsetTop)/2) {
-        nav_a[2].classList.add('longer')
-    }
-    else if (scroll_now <= (section05.offsetTop + section04.offsetTop)/2) {
-        nav_a[3].classList.add('longer')
-    }
-    else if (scroll_now <= (section06.offsetTop + section05.offsetTop)/2) {
-        nav_a[4].classList.add('longer')
-    }
-    else {
-        nav_a[5].classList.add('longer')
+        for (let index = 0; index < nav_a.length; index++) {
+            const element = nav_a[index];
+            nav_a[index].classList.remove('longer')
+        }
+    
+        var scroll_now = document.documentElement.scrollTop
+    
+        if (scroll_now <= section02.offsetTop/2) {
+            nav_a[0].classList.add('longer')
+        }
+        else if (scroll_now <= (section03.offsetTop + section02.offsetTop)/2) {
+            nav_a[1].classList.add('longer')
+        }
+        else if (scroll_now <= (section04.offsetTop + section03.offsetTop)/2) {
+            nav_a[2].classList.add('longer')
+        }
+        else if (scroll_now <= (section05.offsetTop + section04.offsetTop)/2) {
+            nav_a[3].classList.add('longer')
+        }
+        else if (scroll_now <= (section06.offsetTop + section05.offsetTop)/2) {
+            nav_a[4].classList.add('longer')
+        }
+        else {
+            nav_a[5].classList.add('longer')
+        }
     }
 }
+
+
+
+
 
 $(function(){
     checkName();
@@ -52,6 +57,7 @@ $(function(){
     if (document.body.clientWidth > 768) {
         $('#navBig').show();
         $('#nav_phone').hide();
+        activeNav();
     } else {
         $('#nav_phone').show();
         $('#navBig').hide();
@@ -67,11 +73,42 @@ $(function(){
         if (reWidth > 768) {
             $('#nav_phone').hide();
             $('#navBig').show();
+            activeNav();
         }else{
             $('#nav_phone').show();
             $('#navBig').hide();
         }
-    });
+    }); 
+
+    //天氣卡
+    fetch('https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-B5282D9D-8FDD-40E9-AD48-B1DF3270465D')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(Weather) {
+            console.log(Weather);
+            var ChangHua = Weather.records.location[20].weatherElement  //彰化地區
+            var PoP = ChangHua[0].time[0].parameter.parameterName    //降雨機率
+            var MinT = ChangHua[2].time[0].parameter.parameterName   //最低溫度
+            var MaxT = ChangHua[4].time[0].parameter.parameterName   //最高溫度
+
+            var weatherImg
+            if (PoP < 1) {
+                weatherImg = 'https://i.pinimg.com/originals/fb/c1/f3/fbc1f34bc9c42bc68460798eb6e7b462.jpg'
+            } else if (PoP < 50) {
+                weatherImg = 'https://cms-assets.tutsplus.com/uploads/users/16/posts/30551/final_image/cloud850.png'
+            } else{
+                weatherImg = 'https://d1nhio0ox7pgb.cloudfront.net/_img/v_collection_png/512x512/shadow/cloud_rain.png'
+            }
+
+            $('#weatherCard').append(`
+                <h5>彰化縣天氣</h5>
+                <div class="WeatherPhoto photo_center" style="background-image: url(${weatherImg});"></div>
+                <p>溫度:${MinT} ~ ${MaxT}度</p>`);
+
+            console.log(PoP,MinT,MaxT);
+            //調整天氣狀況圖片CSS部分
+        });
 
 });
 
@@ -85,16 +122,3 @@ function checkName(){
         }
         });
 };
-
-
-// for (let index = 0; index < list.length; index++) {
-//     const element = list[index];
-//     list[index].classList.remove('hidden')
-// }
-
-// for (let index = 0; index < list.length; index++) {
-//     const lists = list[index];
-//     lists.onclick = function () {
-//         list[index].classList.add('hidden')
-//     }
-// }
